b35a6d281c7c430ff6431399ef56718e50053961
BernH4 <bernhard.steidl14mail.com>
2020-05-18 19:43:50 +0200

    Small CSS Change

--- script.js
+++ script.js
@@ -1,59 +1,97 @@
-debug();
-let p = 0;
-    let c = 0;
-    for(let i = 0; i < 5; i++) {
-      let roundResult = playRound(playerPlay(), computerPlay());
-      if (roundResult == "playerWon")  {p++; console.log('%c Player won round!', 'color: green;');}
-      if (roundResult == "playerLost") {c++; console.log('%c Player lost round!', 'color: red;');}
-      if (roundResult == "even")  {console.log('%c Even!', 'color: orangel;');}
+// IF YOU FOUND THIS PROJECT ON TOP AND YOU ARE SEARCHING FOR GOOD RESULUTIONS
+// I JUST WANT YOU TO KNOW THERE ARE WAY BETTER SOLUTIONS, THIS IS JUST MY FIRST TRY
+// YOU SHOULD LOOK FOR OTHER USERS WHO COMMITED THEIR PROJECT IF 
+// YOU WANT TO LEARN/SEE GOOD PRACTICES ;)
+
+let pscore = document.getElementById("pscore");
+let cscore = document.getElementById("cscore");
+let resultWon = document.getElementById("result-won");
+let resultLost = document.getElementById("result-lost");
+let even = document.getElementById("even");
+
+let waitForReset = 0;
+
+buttons = document.querySelectorAll('button')
+buttons.forEach(button => {
+    button.addEventListener('click', playRound)
+});
+
+resultWon.addEventListener('click', reset);
+resultLost.addEventListener('click', reset);
+
+function reset(e){
+    pscore.innerText = "0";
+    cscore.innerText = "0";
+    waitForReset = 0;
+    e.target.style.display = "none";
+    //console.log("clicked")
+}
+
+function playRound(e){
+    even.style.display = "none" 
+    if (waitForReset == 0) {
+    console.log("ps= " + e.target.getAttribute("data-key"));
+    let playerWins = bothPlay(e.target.getAttribute("data-key"), computerPlay());
+    if (playerWins == 1) {
+        pscore.innerText = +(pscore.innerText) + 1;
     }
-    if      (p > c)   console.log(`The round is over, you won! Results: You: ${p} Computer: ${c}`);
-    else if (p < c)   console.log(`The round is over, you sadly lost! Results: You: ${p} Computer: ${c}`);
-    else if (p == c)  console.log(`The round is over, nobody won! Results: You: ${p} Computer: ${c}`);
-
-
-
-
-
-
-    //generate random nummer between "min" and "max"
-    function random(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min +1)) + min; 
-    }
-    //Player chooses "Rock, Paper or Siccors" and returns choosen object
-    function playerPlay() {
-      const buttons = document.querySelectorAll('button');
-      buttons.forEach((button) => {
-          button.addEventListener('click', (e) => {
-              if (button.id = "stein") alert("rock!!!");
-          });
-      });
-    }
-
-
-    //Computer chooses randomly "Rock", "Paper" or "Scissors"
-    function computerPlay() {
-      let itemnumber = random(0,2);
-      //console.log(itemnumber);
-
-      let computerSelection;
-      switch (itemnumber)  {
-        case 0:
-          console.log("ComputerSelection: rock");
-          return "rock";
-          break;
-        case 1:
-        console.log("ComputerSelection: paper");
-          return "paper";
-          break;
-        case 2: 
-          console.log("ComputerSelection: scissors");
-          return "scissors";
-          break;
-      }
-    }
+    else if (playerWins == 0) cscore.innerText = +(cscore.innerText) + 1;
     
+    waitForReset = checkWin();
+}
+};
 
-  
+//display block funktioniert nicht
+function checkWin(){
+    console.log(pscore.textContent,cscore.textContent);
+    if (pscore.textContent === '5') { 
+      resultWon.style.display = "block" 
+      return 1;
+    }  
+    else if (cscore.textContent === '5') {
+      resultLost.style.display = "block"
+      return 1;
+    }
+    else return 0;
+};
+
+//Computer chooses randomly "Rock", "Paper" or "Scissors"
+function computerPlay() {
+return random(0,2);
+
+}
+
+function random(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+let returnvalue = Math.floor(Math.random() * (max - min +1)) + min; 
+console.log("cs= "+ returnvalue)
+return returnvalue;
+};
+//ps = playerSelection, cs = computerSelection PlayerWon == returnvalue = 1 //NOT WORKING CORRECT YET//
+function bothPlay(ps,cs) {
+ //   if (ps == cs) return -1;
+ //   else if ((ps == 0) && cs == 2) return 0;
+ //   else if (ps > cs) return 0;
+ //   else return 1;
+ //}
+    if (ps == cs) {
+        even.textContent = `Ihr habt beide das gleiche gewählt!`
+        even.style.display = "block" 
+        return -1//return `Nobody won! You both choosed ${ps}`
+    }
+    if (ps == 0) {
+        if (cs == 2)   return 0;//return "You lost! Paper beats Rock.";
+        if (cs == 1) return 1;//return "You won! Rock beats Scissors";
+    }
+
+    if (ps == 2) {
+        if (cs == 0)    return 1;//return "You won! Paper beats Rock.";
+        if (cs == 1) return 0;//return "You lost! Scissors beats Paper";
+    }
+
+    if (ps == 1) {
+        if (cs == 0)   return 0;//return "You lost! Rock beats Scissors.";
+        if (cs == 2)  return 1;//return "You won! Scissors beats Paper";
+    }
+}
